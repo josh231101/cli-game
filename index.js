4c465f859ed9d7bc4cb6b7e6dea14e6bdfdeb511
Josue Arreola <josue@heroguest.com>
2023-04-18 23:40:36 -0600

    Initial commit cli game

---
+++ index.js
@@ -0,0 +1,92 @@
+
+
+import chalk from 'chalk';
+import inquirer from 'inquirer';
+import gradient from 'gradient-string';
+import chalkAnimation from 'chalk-animation';
+import figlet from 'figlet';
+import { createSpinner } from 'nanospinner';
+
+
+let playerName;
+
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+
+async function welcome() {
+  const rainbowTitle = chalkAnimation.rainbow(
+    'How well do you know me? \n'
+  );
+
+  await sleep();
+  rainbowTitle.stop();
+
+  console.log(`
+    ${chalk.bgBlue('HOW TO PLAY')} 
+    I am a process on your computer.
+    If you get any question wrong I will be ${chalk.bgRed('killed')}
+    So get all the questions right...
+
+  `);
+}
+
+async function askName() {
+    const answers = await inquirer.prompt({
+      name: 'player_name',
+      type: 'input',
+      message: 'What is your name?',
+      default() {
+        return 'Player';
+      },
+    });
+  
+    playerName = answers.player_name;
+  }
+
+  async function question1() {
+    const answers = await inquirer.prompt({
+      name: 'question_1',
+      type: 'list',
+      message: 'What is my favorite animal?\n',
+      choices: [
+        'Dogs 🐶',
+        'Guinea pigs 🐹',
+        'Rabbits🐰',
+        'Cats 🐱',
+      ],
+    });
+  
+    return handleAnswer(answers.question_1 === 'Rabbits🐰');
+  }
+
+  async function handleAnswer(isCorrect) {
+    const spinner = createSpinner('Checking answer...').start();
+    await sleep();
+  
+    if (isCorrect) {
+      spinner.success({ text: `Nice work ${playerName}. That's a legit answer` });
+    } else {
+      spinner.error({ text: `💀💀💀 Game over, you lose ${playerName}!` });
+      process.exit(1);
+    }
+  }
+
+  function winner() {
+    console.clear();
+    figlet(`Congrats , ${playerName} !`, (err, data) => {
+      console.log(gradient.pastel.multiline(data) + '\n');
+
+      console.log(
+        chalk.green(
+          `I owe you your favorite thing in the world`
+        )
+      );
+  
+      process.exit(0);
+    });
+  }
+
+console.clear();
+await welcome();
+await askName();
+await question1();
+winner();
